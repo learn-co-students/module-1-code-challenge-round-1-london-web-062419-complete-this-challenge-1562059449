780ef462195d446d67162bc8e6575c98e4f2a3c8
github username <github email address>
2019-07-02 11:36:45 +0100

    Mod-1-code-challenge

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,31 @@
 class Review
+
+attr_accessor :content 
+attr_accessor :customer, :restaurant, :rating
+
+@@all = []
+
+def initialize(customer, restaurant, content, rating)
+        @customer = customer
+        @restaurant = restaurant
+        @content = content
+        if rating.between?(1, 5)
+            @rating = rating
+        elsif rating > 5
+            @rating = 5
+        elsif rating < 1
+            @rating = 1
+        end
+        @@all << self
+
+        #testing the above I don't know if the else statement will work. 
   
 end
 
+
+def self.all
+    @@all
+end
+
+
+end
--- tools/console.rb
+++ tools/console.rb
@@ -6,6 +6,16 @@ end
 # Insert code here to run before hitting the binding.pry
 # This is a convenient place to define variables and/or set up new object instances,
 # so they will be available to test and play around with in your console
+c1 = Customer.new("david", "bad")
+c2 = Customer.new("mani", "best")
 
+r1 = Restaurant.new("blecker")
+r2 = Restaurant.new("fiveguys")
+
+rw0 = Review.new(c1, r1, "decent", 4)
+rw1 = c1.add_review("blecker", "alright", 3)
+rw2 = c1.add_review("fiveguys", "good", 4)
+rw3 = c2.add_review("fiveguys", "good", 4)
+rw4 = c2.add_review("fiveguys", "ok", 2)
 binding.pry
 0 #leave this here to ensure binding.pry isn't the last line
